3594af7cb93081e90ed6ac65c543d5389ab50a5f
allenlipeng47 <allen.lipeng47@gmail.com>
2015-02-16 23:44:28 -0800

    ng-repeat tag was wrong, corrected

--- angular/test/unit/controllersSpec.js
+++ angular/test/unit/controllersSpec.js
@@ -3,10 +3,10 @@ describe('PhoneListCtrl', function(){
   beforeEach(module('phonecatApp'));
 
   it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    var scope = {}, ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
     expect(scope.phones.length).toBe(3);
+	expect(true).toBe(true);
   }));
 
 });
